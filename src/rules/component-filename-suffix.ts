8554445308752ab893f8972b51f73e007d4e64fd
l08084 <l08084.1989@gmail.com>
2021-07-11 16:24:29 +0900

    update

---
+++ src/rules/component-filename-suffix.ts
@@ -0,0 +1,34 @@
+import { TSESLint } from '@typescript-eslint/experimental-utils';
+
+const STYLE_GUIDE_LINK = 'https://angular.io/styleguide#style-02-03';
+
+export const componentFilenameSuffix: TSESLint.RuleModule<
+  'componentFilenameSuffix',
+  []
+> = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      category: 'Best Practices',
+      description: `Classes decorated with @Component must have suffix "Component" (or custom) in their name. See more at ${STYLE_GUIDE_LINK}`,
+      recommended: 'error',
+      url: 'https://github.com/l08084/eslint-plugin-ionic-tappable/blob/main/docs/rules/click-events-have-tappable.md',
+    },
+    messages: {
+      componentFilenameSuffix: `Component class names should end with one of these suffixes: {{suffixes}} (${STYLE_GUIDE_LINK})`,
+    },
+    schema: [],
+  },
+  create: (context) => {
+    return {
+      'ClassDeclaration > Decorator[(expression.callee.name = "Component")]'(
+        node
+      ) {
+        console.log(node);
+      },
+    };
+  },
+};
+
+module.exports = componentFilenameSuffix;
+export default componentFilenameSuffix;
